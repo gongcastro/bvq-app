0afb446754cb81c627d0e351f3cd36ffa3a8b4dd
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-03-09 14:31:11 +0100

    Ignore _targets/ folder

---
+++ R/bvq.R
@@ -0,0 +1,59 @@
+#' Get BVQ data
+#' @param ... Arguments to be passed to \code{bvq_responses}
+#' @returns A named list of data frames containing questionnaire responses, participant data, and item data from BVQ
+get_bvq <- function(...) {
+    
+    bvq_connect()
+    
+    # get participant data
+    p <- bvq_participants()
+    
+    # get questionnaire responses
+    r <- bvq_responses(p)
+    
+    # merge participant data with questionnaire responses
+    edu_levels <- c("noeducation" = "No education",
+                    "primary" = "Primary",
+                    "secondary" = "Secondary",
+                    "complementary" = "Complementary",
+                    "vocational" = "Vocational",
+                    "university" = "University")
+    
+    l <- bvq_logs(p, r) |>
+        mutate(id_bvq = id) |> 
+        mutate(
+            across(starts_with("edu_"), ~ as.numeric(
+                factor(.x, levels = names(edu_levels), ordered = TRUE)
+            )),
+            # get maximum educational attainment of parents
+            edu_parent = apply(cbind(edu_parent1, edu_parent2), 1, max, na.rm = FALSE),
+            # recode it as factor
+            edu_parent = factor(edu_parent, levels = 1:6, labels = edu_levels)
+        ) |>
+        select(id, id_bvq, time, time_stamp,
+               age, lp, dominance, edu_parent,
+               version, completed,
+               doe_catalan, doe_spanish, doe_others)
+    
+    pool <- bvqdev::pool |>
+        mutate(xsampa = flatten_sampa(xsampa),
+               ipa = xsampa(xsampa, "ipa"))
+    
+    v <- bvq_vocabulary(p, r)
+    
+    n <- bvq_norms(p, r)
+    
+    # get list of all relevant datasets
+    bvq_data <- list(participants = p,
+                     responses = r,
+                     logs = l,
+                     pool = pool,
+                     vocabulary = v,
+                     norms = n)
+    
+    attr(bvq_data, "updated") <- Sys.time()
+    
+    saveRDS(bvq_data, "bvq-app/data/bvq.rds")
+    
+    return(bvq_data)
+}
